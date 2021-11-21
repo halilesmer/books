da0376be42f9eef062fd7744e1570602e31a9d6f
halilesmer <taximinator@gmail.com>
2021-11-21 18:36:01 +0100

    Provider --> Consumer

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,16 @@
 import React from "react";
 import Booklist from "./Booklist.js";
-
+import BookContextProvider from "../BookContext";
 
 class App extends React.Component {
-  
   render() {
     return (
       <>
-        
-            <Booklist/>
+        <BookContextProvider>
+          <Booklist />
+        </BookContextProvider>
       </>
     );
   }
 }
 export default App;
-
--- src/components/Booklist.js
+++ src/components/Booklist.js
@@ -1,36 +1,34 @@
 import React from "react";
 import './Booklist.css';
 import Book from "./Book.js";
-import BookContext from "../BookContext";
+import { BookContext } from "../BookContext";
 
 class Booklist extends React.Component {
   
-  static contextType = BookContext;
   
   render() {
-    const books = this.context;
-    console.log('BookContext: ', this.context);
-  
-    /* const bookList = this.props.books.map((book, i) => {
-      //console.log('book: ', book);
-      return <Book book={book} key={i} />;
-    }); */
     return (
-      <section className="page-section bg-light" id="portfolio">
-        <div className="container">
-          <div className="text-center">
-            <h2 className="section-heading text-uppercase">BookFolio</h2>
-            <h3 className="section-subheading text-muted">
-              Lorem ipsum dolor sit amet consectetur.
-            </h3>
-          </div>
-          <div className="row">
-            {books.map((book, i) => {
-              return <Book book={book} key={i} />;
-            })}
-          </div>
-        </div>
-      </section>
+      <BookContext.Consumer>
+        {(value) => {
+          return (
+            <section className="page-section bg-light" id="portfolio">
+              <div className="container">
+                <div className="text-center">
+                  <h2 className="section-heading text-uppercase">BookFolio</h2>
+                  <h3 className="section-subheading text-muted">
+                    Lorem ipsum dolor sit amet consectetur.
+                  </h3>
+                </div>
+                <div className="row">
+                  {value.books.map((book, i) => {
+                    return <Book book={book} key={i} />;
+                  })}
+                </div>
+              </div>
+            </section>
+          );
+        }}
+      </BookContext.Consumer>
     );
   }
 }
