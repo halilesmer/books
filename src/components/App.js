ad69cf0dcfd332669b18b2ddd45b4445ae89af89
halilesmer <taximinator@gmail.com>
2021-11-21 20:02:22 +0100

    Multiple Context  +ThemeContext

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import Booklist from "./Booklist.js";
 import BookContextProvider from "../BookContext";
+import ThemeContextProvider from "../ThemeContext";
 
 class App extends React.Component {
   render() {
     return (
       <>
-        <BookContextProvider>
-          <Booklist />
-        </BookContextProvider>
+        <ThemeContextProvider>
+          <BookContextProvider>
+            <Booklist />
+          </BookContextProvider>
+        </ThemeContextProvider>
       </>
     );
   }
--- src/components/Booklist.js
+++ src/components/Booklist.js
@@ -1,52 +1,48 @@
 import React from "react";
-import './Booklist.css';
-import Book from "./Book.js";
-import { BookContext } from "../BookContext";
+import "./BookList.css";
+import Book from "./Book";
+import { BookContext } from "../BookContext.js";
+import { ThemeContext } from "../ThemeContext.js";
 
-class Booklist extends React.Component {
-  
-  
+class BookList extends React.Component {
   render() {
     return (
-      <BookContext.Consumer>
-        {(value) => {
-          return (
-            <section className="page-section bg-light" id="portfolio">
-              <div className="container">
-                <div className="text-center">
-                  <h2 className="section-heading text-uppercase">BookFolio</h2>
-                  <h3 className="section-subheading text-muted">
-                    Lorem ipsum dolor sit amet consectetur.
-                  </h3>
-                </div>
-                <div className="row">
-                  {value.books.map((book, i) => {
-                    return <Book book={book} key={i} />;
-                  })}
-                </div>
-              </div>
-            </section>
-          );
-        }}
-      </BookContext.Consumer>
+      <ThemeContext.Consumer>
+        {(contextTheme) => (
+          <BookContext.Consumer>
+            {(contextBook) => {
+              const { books } = contextBook;
+              const { isDarkTheme, dark, light } = contextTheme;
+              const theme = isDarkTheme ? dark : light;
+
+              return (
+                <section
+                  className="page-section"
+                  style={{ background: theme.bg, color: theme.txt }}
+                  id="portfolio"
+                >
+                  <div className="container">
+                    <div className="text-center">
+                      <h2 className="section-heading text-uppercase">
+                        BookFolio
+                      </h2>
+                      <h3 className="section-subheading text-muted">
+                        Lorem ipsum dolor sit amet consectetur.
+                      </h3>
+                    </div>
+                    <div className="row">
+                      {books.map((book, i) => {
+                        return <Book book={book} key={i} />;
+                      })}
+                    </div>
+                  </div>
+                </section>
+              );
+            }}
+          </BookContext.Consumer>
+        )}
+      </ThemeContext.Consumer>
     );
   }
 }
-export default Booklist;
-
-
-/* <section className="page-section bg-light" id="portfolio">
-        <div className="container">
-          <div className="text-center">
-            <h2 className="section-heading text-uppercase">BookFolio</h2>
-            <h3 className="section-subheading text-muted">
-              Lorem ipsum dolor sit amet consectetur.
-            </h3>
-          </div>
-          <div className="row">
-            {books.map((book, i) => {
-              return <Book book={book} key={i} />;
-            })}
-          </div>
-        </div>
-      </section> */
+export default BookList;
